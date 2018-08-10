2786037a2b4e3d539d3424271304033cfd460c57
Amanda Stott <amanda.scott@cox.com>
2018-08-10 15:26:40 -0400

    added honeypot.R file to scripts

--- scripts/honeypot.R
+++ scripts/honeypot.R
@@ -3,7 +3,7 @@
 #-----------------------------------------------------------------------------
 
 library(data.table)
-honeypot <- read_csv("~/Development/data_viz/input/marx-geo.csv")
+honeypot <- read_csv("~/Development/data_viz/input/marx_geo.csv")
 honeypot <- data.table(honeypot)
 
 #-----------------------------------------------------------------------------
@@ -20,36 +20,53 @@ library(forcats)
 # Clean Data 
 #-----------------------------------------------------------------------------
 
-honeypot_data <- data.table(as.POSIXct.Date(length(honeypot$datetime)),
-                              character(length(honeypot$host)),
+# Remove N/A & Extraneous Values 
+
+honeypot <- data.frame(honeypot %>% 
+                         filter(!is.na(latitude) & !is.na(longitude),
+                                latitude <= 100))
+
+honeypot_data <- data.table(character(length(honeypot$host)),
                               character(length(honeypot$src)),
                               character(length(honeypot$proto)),
                               character(length(honeypot$latitude)),
                               character(length(honeypot$longitude)))
 
-honeypot_data[,1] <- honeypot$datetime
-honeypot_data[,2] <- honeypot$host
-honeypot_data[,3] <- honeypot$src
-honeypot_data[,4] <- honeypot$proto
-honeypot_data[,5] <- honeypot$latitude
-honeypot_data[,6] <- honeypot$longitude
+honeypot_data[,1] <- honeypot$host
+honeypot_data[,2] <- honeypot$src
+honeypot_data[,3] <- honeypot$proto
+honeypot_data[,4] <- honeypot$latitude
+honeypot_data[,5] <- honeypot$longitude
 
-colnames(honeypot_data) <- c("timestamp", "host", "src", "proto", "latitude", "longitude")
+colnames(honeypot_data) <- c("host", "src", "proto", "latitude", "longitude")
 
-honeypot_data <- na.omit(honeypot_data)
+#-----------------------------------------------------------------------------
+# Aggregate Data For Further Cleaning
+#-----------------------------------------------------------------------------
+
+geo_grouping <- as.data.frame(honeypot_data) %>%
+  group_by(latitude, longitude, host, src, proto) %>%
+  summarise(count = n())
+
+top_hundred <- data.table(head(sort(geo_grouping$count, decreasing = T), 100))
+
+colnames(top_hundred) <- c("count")
+
+sticky_pot <- inner_join(geo_grouping, top_hundred, by = "count")
 
 #-----------------------------------------------------------------------------
 # Write to file
 #-----------------------------------------------------------------------------
 
-write.csv(honeypot_data, file = "honeypot.csv") 
+write.csv(sticky_pot, file = "honeypot.csv") 
 
 #-----------------------------------------------------------------------------
 # Write to Mongo 
 #-----------------------------------------------------------------------------
 
 honeypot_collection = mongo(collection = "honeyPot", db = "dataViz") # create connection, database and collection
-honeypot_collection $insert(honeypot_data)
+honeypot_collection $insert(sticky_pot)
+
 
 
 
